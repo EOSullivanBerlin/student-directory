a0207fd04e14704fcfed5aef6130743b8f95b62e
Eoin O Sullivan <esully666@gmail.com>
2017-06-19 19:54:10 +0000

    First letter is F or f

--- directory.rb
+++ directory.rb
@@ -1,29 +1,12 @@
 
-
-
-
- students = [
- {name: "Dr. Hannibal Lecter", cohort: :november},
- {name: "Darth Vader", cohort: :november},
- {name: "Nurse Ratched", cohort: :november},
- {name: "Michael Corleone", cohort: :november},
- {name: "Alex DeLarge", cohort: :november},
- {name: "The Wicked Wich of the West", cohort: :november},
- {name: "Therminator", cohort: :november}, 
- {name: "The Joker", cohort: :november},
- {name: "Joffery Bartheon", cohort: :november},
- {name: "Norman Bates", cohort: :november}
-]
-
-
 def print_header
     puts "The students of Villains Academy"
     puts "-----------------"
 end
 
 def print(students)
-    students.each do |student|
-        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    students.each_with_index do |student, index|
+        puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)" if student[0] == "F" || "f"
     end
 end
 
