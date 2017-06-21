37441c3ce0f9a4e7c1f090d62881f4e096c14bbf
Eoin O Sullivan <esully666@gmail.com>
2017-06-21 15:54:02 +0000

    We can add more information

--- directory.rb
+++ directory.rb
@@ -7,7 +7,7 @@ end
 def print(students)
     
     students.each_with_index do |student, index|
-        puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < 12
+        puts "#{index + 1}. #{student[:name]}, From: #{student[:country]}, Hobbie: #{student[:hobbie]} (#{student[:cohort]} cohort)" if student[:name].length < 12
    
     end
 end
@@ -20,19 +20,37 @@ end
 
 
 def input_students
-    puts "Please enter the names of the students"
-    puts "To finish, just hit return twice"
-    # create an empty array
     students = []
+
+  
+    # the prock will ask you to "Please enter the #{argument}"
+    # call the prock
+    puts "Please enter the names, country of birth and hobbies of the student"
+    puts "Strat with there name."
+    puts "To finish, just hit return a number of times"
+    # create an empty array
+    
     #get the first name
-    name = gets.chomp
+     name = gets.chomp
+    puts "Add the students country of birth"
+        birth = gets.chomp
+        puts "Add the students hobbie"
+        hobbie = gets.chomp
+    
+    
     #while the name is not empty, repeat this code
-    while !name.empty? do
+    while !name.empty?  do
         #add the students hash to the array
-        students << {name: name, cohort: :november}
+        students << {name: name, cohort: :november, country: birth, hobbie: hobbie}
+        puts "Please enter the next students details starting with their name"
+        name = gets.chomp
+        puts "Add the students country of birth"
+        birth = gets.chomp
+        puts "Add the students hobbie"
+        hobbie = gets.chomp
         puts "Now we have #{students.count} students"
         #get another name from the user
-        name = gets.chomp
+        
     end
     # return the array of students
     students
