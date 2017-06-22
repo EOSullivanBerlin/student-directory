b92bf864712a2d1b58c84b8691b51b3a0edbfef6
Eoin O Sullivan <esully666@gmail.com>
2017-06-22 16:54:16 +0000

    Save feature added

--- interactive_menu.rb
+++ interactive_menu.rb
@@ -1,4 +1,18 @@
-@students =[] # ==> Every method now has acess to this array
+@students = [] # ==> Every method now has acess to this array
+
+def save_students
+    #open the file for writing
+    file = File.open("students.csv", "w")
+    #iterate over the array of students
+    @students.each do |student|
+        student_data = [student[:name], student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+    end
+    file.close
+end
+
+
 
 def print_header
     titel = "The students of Villains Academy"
@@ -127,6 +141,7 @@ end
 def print_menu
      puts "1. Input the students"
      puts "2. Show the students"
+     puts "3. Save the list to students.csv"
      puts "9. Exit"
 end
 
@@ -139,6 +154,8 @@ def process_selection
                 input_students
             when  "2"
                 show_students
+            when "3"
+                save_students
         # 4. Repeat from step one
             when "9"
                 exit
